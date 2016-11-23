06bdb033509f73981c938bae40ade129a054355f
JasperRuigrok <JasperRuigrok@Fiorettileerling.nl>
2016-11-23 21:06:26 +0100

    Layout verandert

--- index.php
+++ index.php
@@ -1,6 +1,59 @@
 <!DOCTYPE html>
 <html>
-  <h1>Fioretti college</h1>
-  <p>Welkom op de beroeptinder</p>
-  <a href="inloggen.php">Klik hier om in te loggen.</a>
+<head>
+<style>
+
+#titel {
+position: fixed;
+top: 10px;
+left: 300px;
+font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+color: #017f92;		
+}
+
+#rectangle {
+	position: fixed;
+	top: -20px;
+	left: 0px;
+	width: 220px;
+	height: 8000px;
+	background: #adadad;
+	
+}
+
+#ondertitel {
+position: fixed;
+top: 76px;
+left: 300px;
+font-family: "Arial Narrow", Arial, sans-serif;
+color: #929899;		
+}
+
+#ondertekst {
+position: fixed;
+top: 250px;
+left: 300px;
+font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+color: #black;		
+}
+
+#link {
+position: fixed;
+top: 325px;
+left: 300px;
+font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+		
+}
+	
+			
+</style>
+</head>
+<body>
+
+<p id= "rectangle"></p>
+  <h1 id= "titel" STYLE="font-size: 40pt;">Beroepen Tinder</h1>
+  <p id= "ondertitel" STYLE="font-size: 25pt;">Fioretti college</p>
+  <p id= "ondertekst" STYLE="font-size: 15pt;">Welkom op de beroeptinder.</p>
+  <a href="inloggen.php" id= "link">Klik hier om in te loggen.</a>
+  </body>
 </html>
