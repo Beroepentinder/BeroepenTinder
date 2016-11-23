62a01c60b6be318fde2cc13fdcb1a990489d069c
Lennard van der Plas <LennardvanderPlas@Fiorettileerling.nl>
2016-11-23 18:07:52 +0100

    ff iets veranderd

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 <html>
   <h1>Fioretti college</h1>
   <p>Welkom op de beroeptinder</p>
-  <a href="inloggen.html">Klik hier om in te loggen.</a>
+  <a href="inloggen.php">Klik hier om in te loggen.</a>
 </html>
