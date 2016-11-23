80e576f2507ce48f0a49e14320ba118252a9fc58
Lennard van der Plas <LennardvanderPlas@Fiorettileerling.nl>
2016-11-23 16:58:08 +0100

    Html naar php gezet

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 <html>
   <h1>Fioretti college</h1>
   <p>Welkom op de beroeptinder</p>
-  <a href="beroepen.html">Klik hier om naar de beroepenpagina te gaan!</a>
+  <a href="inloggen.html">Klik hier om in te loggen.</a>
 </html>
