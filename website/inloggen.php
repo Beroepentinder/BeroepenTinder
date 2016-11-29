c8da9997d630e371519857bdfc63d5b415836872
JasperRuigrok <JasperRuigrok@Fiorettileerling.nl>
2016-11-29 23:37:12 +0100

    fout verwijderd
    onnodige stijlen verwijderd, staan al in style.css

--- website/inloggen.php
+++ website/inloggen.php
@@ -22,64 +22,7 @@
 
   </head>
   <main>
-  <style>
-#titel {
-position: fixed;
-top: 10px;
-left: 300px;
-font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-color: #017f92;
-}
-
-#rectangle {
-	position: fixed;
-	top: -20px;
-	left: 0px;
-	width: 220px;
-	height: 8000px;
-	background: #adadad;
-
-}
-
-#ondertitel {
-position: fixed;
-top: 76px;
-left: 300px;
-font-family: "Arial Narrow", Arial, sans-serif;
-color: #929899;
-}
-
-#ondertekst {
-position: fixed;
-top: 250px;
-left: 300px;
-font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-color: #black;
-}
-
-#link {
-position: fixed;
-top: 325px;
-left: 300px;
-font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-}
-
-#Inlogsysteem {
-position: fixed;
-top: 250px;
-left: 300px;
-font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-}
-
-#Foutmelding_inloggen {
-position: fixed;
-top: 340px;
-left: 300px;
-font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-color: #c90000;
-font-size: 75%;
-}
-</style>
+ 
 
   <p id="rectangle"></p>
   <div id="inlogsysteem">
