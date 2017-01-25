e079fba57488ccf8a9a8d2e3e81cc2816e8f839d
JasperRuigrok <JasperRuigrok@Fiorettileerling.nl>
2017-01-25 20:00:18 +0100

    Achtergrond website docenten test
    test afbeelding gebruikt, alleen op index

--- websiteLeraren/index.php
+++ websiteLeraren/index.php
@@ -13,6 +13,6 @@
   <p id= "ondertitel" STYLE="font-size: 25pt;">Fioretti college</p>
   <p id= "ondertekst" STYLE="font-size: 15pt;">Welkom op de beroeptinder.</p>
   <a href="inloggen.php" id= "link">Klik hier om in te loggen.</a>
-
+	<IMG id="Achtergrond_docenten" SRC="http://www.beroepentinder.fiorettileerling.nl/websiteLeraren/Achtergrond_docenten">
 </body>
 </html>
