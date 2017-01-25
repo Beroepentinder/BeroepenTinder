f25082060f8d01f2d704e59cf8aa7a5ba97ee3bb
Lennard van der Plas <LennardvanderPlas@Fiorettileerling.nl>
2017-01-25 22:47:07 +0100

    alpha v1.1
    swipe systeempje werkent.
    meest simpele versie ervan thou

--- websiteLeerlingen/verstuurd.php
+++ websiteLeerlingen/verstuurd.php
@@ -1,32 +1,15 @@
 <!DOCTYPE html>
 <html>
-<head> 
+<head>
 	<title>Page Title</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
 	<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-	
-	<?php
-		session_start();
-		$server = "localhost";
-		$user = "beroepentinder"; //vul hier jouw gebruikersnaam in (leerlingnummer)
-		$pass = "Beroepen123Tinder"; //vul hier jouw wachtwoord in (database1)
-		$db = "beroepentinder"; //vul hier de naam van jouw database in (leerlingnummer)
 
-		// Hier wordt connectie gemaakt met de database
-		$mysql = mysqli_connect($server,$user,$pass,$db) or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
-			
-		if (isset($_GET["gegevens"])) {
-			$gegevens = $_GET["gegevens"];
-		}
-		
-		// Gegevens van producten opvragen uit de database 
-		$imageId = mysqli_query($mysql,"SELECT * FROM Afbeeldinggegevens") or die("De selectquery op de database is mislukt!");
-	
-		// Hier wordt de connectie met de database weer verbroken
-		mysqli_close($mysql) or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
+	<?php
+
 	?>
 </head>
 <body>
@@ -38,15 +21,46 @@
 	</div><!-- /header -->
 
 	<div role="main" class="ui-content">
-		<h1>Je sector is ... !</h1>
-		<p>Nummers van plaatjes die je hebt aangevinkt</p>
 		<?php
-		$arrayGegevens = explode(",",$gegevens);
-		
-		$gegevens = array($gegevens);
-		foreach($gegevens as $d){
-			echo $d;
+		session_start();
+		$server = "localhost";
+		$user = "beroepentinder"; //vul hier jouw gebruikersnaam in (leerlingnummer)
+		$pass = "Beroepen123Tinder"; //vul hier jouw wachtwoord in (database1)
+		$db = "beroepentinder"; //vul hier de naam van jouw database in (leerlingnummer)
+
+		// Hier wordt connectie gemaakt met de database
+		$mysql = mysqli_connect($server,$user,$pass,$db) or die("Fout: Er is geen verbinding met de MySQL-server tot stand gebracht!");
+
+		if (isset($_GET["gegevens"])) {
+			$gegevens = $_GET["gegevens"];
 		}
+
+
+
+		$arrayGegevens = explode(",",$gegevens);
+		foreach($arrayGegevens as $d){
+			$d = intval($d);
+			$sectorId = mysqli_query($mysql, "SELECT sectorId FROM Afbeeldinggegevens WHERE imagesId=$d");
+			list($temp) = mysqli_fetch_row($sectorId);
+			$stack[] = $temp;
+		}
+		$x = $stack;
+		$ar = array_replace($x,array_fill_keys(array_keys($x, null),''));
+
+		$counts = array_count_values($ar);
+		arsort($counts);
+
+		$top_with_count = array_slice($counts, 0, 1, true);
+		$top = array_keys($top_with_count);
+
+		$topId = $top[0];
+		$sectorNameArray = mysqli_query($mysql, "SELECT SectorNaam FROM Sectoren WHERE SectorID=$topId");
+
+		list($sectorNameString) = mysqli_fetch_row($sectorNameArray);
+		echo "<h1>Jouw sector is {$sectorNameString}!</h1>";
+
+		// Hier wordt de connectie met de database weer verbroken
+		mysqli_close($mysql) or die("Het verbreken van de verbinding met de MySQL-server is mislukt!");
 		?>
 	</div>
 
