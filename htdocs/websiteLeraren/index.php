7274f4c381e20bed8c2b1ccd9c3a0f73d9b15eee
Lennard van der Plas <LennardvanderPlas@Fiorettileerling.nl>
2017-01-26 17:45:45 +0100

    required toegevoegt

---
+++ htdocs/websiteLeraren/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<?php 
+header('Refresh: 0; url=inloggen.php');
+?>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="style.css"></link>
+	<link rel="icon" href="faviconvlamtr.gif" type="image/gif" >
+  
+</head>
+<body>
+
+	<p id= "rectangle"></p>
+  <h1 id= "titel" STYLE="font-size: 40pt;">Beroepen Tinder</h1>
+  <p id= "ondertitel" STYLE="font-size: 25pt;">Fioretti college</p>
+  <p id= "ondertekst" STYLE="font-size: 15pt;">Welkom op de beroeptinder.</p>
+  <a href="inloggen.php" id= "link">Klik hier om in te loggen.</a>
+<IMG id="Achtergrond_docenten" SRC="http://www.beroepentinder.fiorettileerling.nl/websiteLeraren/Achtergrond_docenten.png">
+</body>
+</html>
