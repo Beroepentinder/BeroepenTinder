b62b9d73387e91a1ebd3c780dc9bf4537eca8ff4
Joey Minck <JoeyMinck@Fiorettileerling.nl>
2017-02-01 10:55:02 +0100

    favicon + titel

--- websiteLeerlingen/login.php
+++ websiteLeerlingen/login.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head> 
-	<title>Page Title</title>
+	<link rel="stylesheet" type="text/css" href="style.css"></link>
+	<link rel="icon" href="faviconvlamtr.gif" type="image/gif" >
+
+	<title>BeroepenTinder</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
