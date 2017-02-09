9d5d9b453a65780a39043c5d0cea8fd06cba1aa0
Joey Minck <JoeyMinck@Fiorettileerling.nl>
2017-02-09 12:46:30 +0100

    rectangle

--- websiteLeraren/index.php
+++ websiteLeraren/index.php
@@ -13,7 +13,7 @@ header('Refresh: 0; url=inloggen.php');
 </head>
 <body>
 
-	<p id= "rectangle"></p>
+  <p id="rectangle" style="border:7px solid #000;"></p>
   <h1 id= "titel" STYLE="font-size: 40pt;">Beroepen Tinder</h1>
   <p id= "ondertitel" STYLE="font-size: 25pt;">Fioretti college</p>
   <p id= "ondertekst" STYLE="font-size: 15pt;">Welkom op de Beroepentinder.</p>
